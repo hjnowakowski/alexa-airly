176d12361873c360a93d1c693be3910467136066
Henryk Nowakowski <hjnowakowski@gmail.com>
2021-04-08 18:18:39 +0200

    Reformat code and Update responses

--- index.js
+++ index.js
@@ -8,137 +8,137 @@ const GeoLocalization = require("./nominatimRequest")
 const PERMISSIONS = ['read::alexa:device:all:address'];
 
 const messages = {
-  WELCOME: 'Welcome to the Sample Device Address API Skill!  You can ask for the device address by saying what is my address.  What do you want to ask?',
-  WHAT_DO_YOU_WANT: 'What do you want to ask?',
-  NOTIFY_MISSING_PERMISSIONS: 'Please enable Location permissions in the Amazon Alexa app.',
-  NO_ADDRESS: 'It looks like you don\'t have an address set. You can set your address from the companion app.',
-  ADDRESS_AVAILABLE: 'Here is your full address: ',
-  ERROR: 'Uh Oh. Looks like something went wrong.',
-  LOCATION_FAILURE: 'There was an error with the Device Address API. Please try again.',
-  GOODBYE: 'Bye! Thanks for using the Sample Device Address API Skill!',
-  UNHANDLED: 'This skill doesn\'t support that. Please ask something else.',
-  HELP: 'You can use this skill by asking something like: whats my address?',
-  STOP: 'Bye! Thanks for using the Sample Device Address API Skill!',
+    WELCOME: 'Welcome!',
+    WHAT_DO_YOU_WANT: 'What do you want to ask?',
+    NOTIFY_MISSING_PERMISSIONS: 'Please enable Location permissions in the Amazon Alexa app.',
+    NO_ADDRESS: 'It looks like you don\'t have an address set. You can set your address from the companion app.',
+    ADDRESS_AVAILABLE: 'Here is your full address: ',
+    ERROR: 'Uh Oh. Looks like something went wrong.',
+    LOCATION_FAILURE: 'There was an error with the Device Address API. Please try again.',
+    GOODBYE: 'Bye! Thanks for using the Air Quality Skill!',
+    UNHANDLED: 'This skill doesn\'t support that. Please ask something else.',
+    HELP: 'You can use this skill to get weather quality information',
+    STOP: 'Bye! Thanks for using the Air Quality Skill!',
 };
 
 
 const LaunchRequest = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
-  },
-  handle(handlerInput) {
-    return handlerInput.responseBuilder.speak(messages.WELCOME)
-        .reprompt(messages.WHAT_DO_YOU_WANT)
-        .getResponse();
-  },
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        return handlerInput.responseBuilder.speak(messages.WELCOME)
+            .reprompt(messages.WHAT_DO_YOU_WANT)
+            .getResponse();
+    },
 };
 
 const GetWeatherPollutionIntent = {
-  canHandle(handlerInput) {
+    canHandle(handlerInput) {
 
-    const { request } = handlerInput.requestEnvelope;
+        const { request } = handlerInput.requestEnvelope;
 
-    return request.type === 'IntentRequest' && request.intent.name === 'GetWeatherPollutionIntent';
+        return request.type === 'IntentRequest' && request.intent.name === 'GetWeatherPollutionIntent';
 
-  },
-  async handle(handlerInput) {
+    },
+    async handle(handlerInput) {
 
-    const { requestEnvelope, serviceClientFactory, responseBuilder } = handlerInput;
+        const { requestEnvelope, serviceClientFactory, responseBuilder } = handlerInput;
 
 
-    const consentToken = requestEnvelope.context.System.user.permissions
-        && requestEnvelope.context.System.user.permissions.consentToken;
-    if (!consentToken) {
-      return responseBuilder
-          .speak(messages.NOTIFY_MISSING_PERMISSIONS)
-          .withAskForPermissionsConsentCard(PERMISSIONS)
-          .getResponse();
-    }
+        const consentToken = requestEnvelope.context.System.user.permissions
+            && requestEnvelope.context.System.user.permissions.consentToken;
+        if (!consentToken) {
+            return responseBuilder
+                .speak(messages.NOTIFY_MISSING_PERMISSIONS)
+                .withAskForPermissionsConsentCard(PERMISSIONS)
+                .getResponse();
+        }
 
-    try {
-      const { deviceId } = requestEnvelope.context.System.device;
-      const deviceAddressServiceClient = serviceClientFactory.getDeviceAddressServiceClient();
-      const address = await deviceAddressServiceClient.getFullAddress(deviceId);
+        try {
+            const { deviceId } = requestEnvelope.context.System.device;
+            const deviceAddressServiceClient = serviceClientFactory.getDeviceAddressServiceClient();
+            const address = await deviceAddressServiceClient.getFullAddress(deviceId);
 
-      let response;
-      if (address.addressLine1 === null && address.stateOrRegion === null) {
-        response = responseBuilder.speak(messages.NO_ADDRESS).getResponse();
-      } else {
-        const geoLocalization = await GeoLocalization.getGeoLocation(address);
+            let response;
+            if (address.addressLine1 === null && address.stateOrRegion === null) {
+                response = responseBuilder.speak(messages.NO_ADDRESS).getResponse();
+            } else {
+                const geoLocalization = await GeoLocalization.getGeoLocation(address);
 
-        const airlyRequest = new AirlyRequest(geoLocalization.lat, geoLocalization.lng);
+                const airlyRequest = new AirlyRequest(geoLocalization.lat, geoLocalization.lng);
 
-        const speakableAirlyOutput = await airlyRequest.getspeakableAirlyOutput();
+                const speakableAirlyOutput = await airlyRequest.getspeakableAirlyOutput();
 
-        return handlerInput.responseBuilder
-            .speak(speakableAirlyOutput)
-            .withSimpleCard(SKILL_NAME)
-            .getResponse();
-      }
-      return response;
-    } catch (error) {
-      if (error.name !== 'ServiceError') {
-        console.log(error);
-        const response = responseBuilder.speak(messages.ERROR).getResponse();
-        return response;
-      }
-      throw error;
-    }
-  },
+                return handlerInput.responseBuilder
+                    .speak(speakableAirlyOutput)
+                    .withSimpleCard(SKILL_NAME)
+                    .getResponse();
+            }
+            return response;
+        } catch (error) {
+            if (error.name !== 'ServiceError') {
+                console.log(error);
+                const response = responseBuilder.speak(messages.ERROR).getResponse();
+                return response;
+            }
+            throw error;
+        }
+    },
 };
 
 const HelpHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'IntentRequest' &&
-      request.intent.name === 'AMAZON.HelpIntent';
-  },
-  handle(handlerInput) {
-    return handlerInput.responseBuilder
-      .speak(HELP_MESSAGE)
-      .reprompt(HELP_REPROMPT)
-      .getResponse();
-  },
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest' &&
+            request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        return handlerInput.responseBuilder
+            .speak(HELP_MESSAGE)
+            .reprompt(HELP_REPROMPT)
+            .getResponse();
+    },
 };
 
 const ExitHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'IntentRequest' &&
-      (request.intent.name === 'AMAZON.CancelIntent' ||
-        request.intent.name === 'AMAZON.StopIntent');
-  },
-  handle(handlerInput) {
-    return handlerInput.responseBuilder
-      .speak(STOP_MESSAGE)
-      .getResponse();
-  },
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest' &&
+            (request.intent.name === 'AMAZON.CancelIntent' ||
+                request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        return handlerInput.responseBuilder
+            .speak(STOP_MESSAGE)
+            .getResponse();
+    },
 };
 
 const SessionEndedRequestHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'SessionEndedRequest';
-  },
-  handle(handlerInput) {
-    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
 
-    return handlerInput.responseBuilder.getResponse();
-  },
+        return handlerInput.responseBuilder.getResponse();
+    },
 };
 
 const ErrorHandler = {
-  canHandle() {
-    return true;
-  },
-  handle(handlerInput, error) {
-    console.log(`Error handled: ${error.message}`);
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`Error handled: ${error.message}`);
 
-    return handlerInput.responseBuilder
-      .speak('Sorry, an error occurred.')
-      .reprompt('Sorry, an error occurred.')
-      .getResponse();
-  },
+        return handlerInput.responseBuilder
+            .speak('Sorry, an error occurred.')
+            .reprompt('Sorry, an error occurred.')
+            .getResponse();
+    },
 };
 
 const SKILL_NAME = 'Space Facts';
@@ -149,11 +149,11 @@ const STOP_MESSAGE = 'Goodbye!';
 const skillBuilder = Alexa.SkillBuilders.standard();
 
 exports.handler = skillBuilder
-  .addRequestHandlers(
-      GetWeatherPollutionIntent,
-    HelpHandler,
-    ExitHandler,
-    SessionEndedRequestHandler
-  )
-  .addErrorHandlers(ErrorHandler)
-  .lambda();
+    .addRequestHandlers(
+        GetWeatherPollutionIntent,
+        HelpHandler,
+        ExitHandler,
+        SessionEndedRequestHandler
+    )
+    .addErrorHandlers(ErrorHandler)
+    .lambda();
